feb36bc49905a788068f3676f9acbec4f4b1b796
Héctor Rosario <36975677+hectorrosario22@users.noreply.github.com>
2025-04-22 22:23:37 -0400

    complete hamming exercise

---
+++ exercism/csharp/hamming/Hamming.cs
@@ -0,0 +1,24 @@
+#pragma warning disable CA1050 // Declare types in namespaces
+public static class Hamming
+#pragma warning restore CA1050 // Declare types in namespaces
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException("Strands must be of equal length.");
+        }
+
+        int distance = 0;
+
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i])
+            {
+                distance++;
+            }
+        }
+
+        return distance;
+    }
+}
